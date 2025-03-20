b071517036dc596ccae253fe564da9f5d50f4fe5
Project516 <138796702+Project516@users.noreply.github.com>
2025-03-19 22:25:35 -0500

    use backup link

--- CompileLatestClient.bat
+++ CompileLatestClient.bat
@@ -2,7 +2,7 @@
 title CompileLatestClient
 mkdir workspace
 cd workspace
-git clone https://git.eaglercraft.rip/eaglercraft/eaglercraft-1.8.git
+git clone https://github.com/stormmasters/eaglercraft-1.8/
 cd eaglercraft-1.8/mcp918
 curl https://piston-data.mojang.com/v1/objects/0983f08be6a4e624f5d85689d1aca869ed99c738/client.jar --output client.jar
 curl https://launchermeta.mojang.com/v1/packages/f6ad102bcaa53b1a58358f16e376d548d44933ec/1.8.json --output 1.8.json
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CompileLatestClient
  
+scripts to compile the latest EaglercraftX client
+
+NOTE: a backup is being used while the offical website is down
